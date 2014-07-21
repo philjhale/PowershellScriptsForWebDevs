9a31356e38ab657d6016d8d965ed60b1b8033e0c
Phil Hale <philjhale@gmail.com>
2014-07-21 11:03:14 +0100

    Script to delete dll and pdb files recursively

---
+++ DeleteAllPdbAndDllFilesFromFolder.ps1
@@ -0,0 +1,4 @@
+﻿# Useful when Visual Studio doesn't remove all pdbs and dlls on rebuild. No idea why this happens but sometimes it does
+
+# Navigate to the folder first. cd path to folder
+gci -recurse -include @("*.dll", "*.pdb") | foreach ($_) { Remove-Item $_.FullName }
