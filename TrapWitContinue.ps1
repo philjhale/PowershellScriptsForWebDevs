c8039752dbf6bca75a77356d2718e2ef928d77ef
Phil Hale <philip.hale@tombola.com>
2015-06-10 11:27:30 +0100

    Simple trap examples

---
+++ TrapWitContinue.ps1
@@ -0,0 +1,10 @@
+﻿Trap {
+  Write-Host "Error happened - $_"
+  Continue # Could also use $ErrorActionPreference = "SilentlyContinue"
+}
+
+Write-Host "Starting script"
+
+$i = $i / 0
+
+Write-Host "This will be shown because of the Continue statement"
---
+++ TrapWithStop.ps1
@@ -0,0 +1,10 @@
+﻿Trap {
+  Write-Host "Error happened - $_"
+  Break # Could also use $ErrorActionPreference = "Stop"
+}
+
+Write-Host "Starting script"
+
+$i = $i / 0
+
+Write-Host "This shouldn't be shown because of the Break statement"
