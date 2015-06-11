22b9f8be174f43f330ae95636a3cacc911c0762a
Phil Hale <philip.hale@tombola.com>
2015-06-11 16:55:13 +0100

    Splatting example

---
+++ Splatting.ps1
@@ -0,0 +1,23 @@
+﻿# https://blogs.endjin.com/2013/05/powershell-best-kept-secrets-splatting/
+
+
+Function DoSomething 
+{ 
+    Param
+    (
+        [string] $Name, 
+        [string] $Description,
+        [string] $Status
+    )
+
+    Write-Host "$Name - $Description - $Status"
+}
+
+$parameters = @{
+    Name = "Phil"
+    Description = "things"
+    Status = "some status"
+}
+
+
+DoSomething @parameters
