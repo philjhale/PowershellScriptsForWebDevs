a4decc4fdef606a564408c7f4a037baa12a726dd
Phil Hale <philip.hale@tombola.com>
2015-08-14 15:28:50 +0100

    Example of spatting if you want to pass a hashtable through an intermediary method without having to include every parameter required for the inner method

---
+++ SplattingWithAnIntermediaryMethod.ps1
@@ -0,0 +1,33 @@
+﻿
+function Log
+{
+ param
+    (
+        [string] $Test1, 
+        [string] $Test2
+    )
+
+    Write-Host "Log called Test1 = $Test1 and Test2 = $Test2"
+}
+
+function CallTestLog
+{
+ param
+    (
+        [string] $SomeParam,
+        [hashtable] $CommonLogParams #Pass as hashtable to intermediary method to avoid bloating the number of parameters
+    )
+
+    Write-Host "CallTestLog called with param $SomeParam"
+    Log @CommonLogParams
+}
+
+$commonLogParams = @{
+    Test1 = "Test param 1"
+    Test2 = "Test param 2"
+}
+
+write-host $commonLogParams.Test1
+
+
+CallTestLog "someparam" $commonLogParams
